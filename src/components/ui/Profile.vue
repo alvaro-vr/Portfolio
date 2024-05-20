deb06c00f0f19b02febaf0b51bdf62c9eb1ac030
Alvaro <alvarovillaresrodriguez@gmail.com>
2024-05-20 18:41:17 +0200

    Componente ToolTip creado + iconos de linkedin y github

--- src/components/ui/Profile.vue
+++ src/components/ui/Profile.vue
@@ -1,7 +1,29 @@
 <template>
-    <div class="h-auto w-full p-[5vh]">
-        <img class="w-full h-full rounded-full border-[1vh] border-yellow-200 border-opacity-30 " 
-        src="../../assets/images/profile.png" alt="Alvaro Villares Rodriguez">
-        <h1 class="text-neutral-700 mt-3 text-4xl font-semibold text-center underline underline-offset-8">Alvaro Villares</h1>
-    </div>
+  <div class="h-auto w-full p-[5vh]">
+    <img
+      class="w-full h-full rounded-full border-[1vh] border-yellow-200 border-opacity-30"
+      src="../../assets/images/profile.png"
+      :alt="data.profile.name"
+    />
+    <h1
+      class="text-neutral-700 mt-3 text-4xl font-semibold text-center underline underline-offset-8"
+    >
+      {{ data.profile.name }}
+    </h1>
+  </div>
+  <div class="flex justify-center h-16 [&>*]:mx-6">
+    <ToolTip :name="'GitHub'">
+      <GitHubIcon class="text-neutral-800" :data="data" />
+    </ToolTip>
+    <ToolTip :name="'Linkedin'">
+      <LinkedinIcon class="text-blue-700" :data="data" />
+    </ToolTip>
+  </div>
 </template>
+
+<script setup>
+import LinkedinIcon from '../icons/LinkedinIcon.vue'
+import GitHubIcon from '../icons/GitHubIcon.vue'
+import ToolTip from '../utils/ToolTip.vue'
+import data from '@/data.json'
+</script>
---
+++ src/components/utils/ToolTip.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="tooltip h-full">
+    <slot></slot>
+    <span class="tooltiptext">{{ name }}</span>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+  name: {
+    type: String
+  }
+})
+</script>
+
+<style scoped>
+.tooltip {
+  position: relative;
+  display: flex;
+  justify-content: center;
+}
+
+.tooltip .tooltiptext {
+  top: 100%;
+  visibility: hidden;
+  width: max-content;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  padding: 5px 10px;
+  border-radius: 6px;
+  position: absolute;
+  z-index: 1;
+  opacity: 0;
+  transform: translateY(-0.8vh);
+  transition:
+    opacity 0.3s ease-in-out,
+    transform 0.3s ease-in-out;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
