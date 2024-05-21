f87eb3d5e026bac332167ccaeb51c7655601cc38
Alvaro <alvarovillaresrodriguez@gmail.com>
2024-05-21 18:46:39 +0200

    detalles del Profile

--- src/components/ui/Profile.vue
+++ src/components/ui/Profile.vue
@@ -1,12 +1,14 @@
 <template>
-  <div id="profile" class="w-full h-screen flex justify-around items-center flex-wrap gap-8 p-10">
+  <div id="profile" class="w-full h-screen flex justify-around items-center flex-wrap gap-10 p-10">
     <img
-      class="md:h-64 lg:h-80 xl:h-3/5 rounded-full border-4 border-neutral-900 border-opacity-30"
+      class="md:h-64 lg:h-80 xl:h-3/5 rounded-full border-4 border-neutral-900 border-opacity-30 shadow-black shadow-2xl"
       src="../../assets/images/profile.png"
       :alt="data.profile.name"
     />
     <div>
-      <h1 class="text-neutral-900 mt-3 text-4xl xl:text-6xl font-medium text-center">
+      <h1
+        class="text-neutral-900 mt-3 text-4xl xl:text-6xl font-medium text-center transition-all duration-1000 hover:scale-150 hover:mb-3"
+      >
         {{ data.profile.name }}
       </h1>
       <h2 class="text-neutral-600 text-xl xl:text-3xl font-medium text-center">
@@ -15,10 +17,10 @@
     </div>
     <div class="flex flex-wrap h-10 lg:h-12 xl:h-16 [&>*]:mx-6">
       <ToolTip :name="'GitHub'">
-        <GitHubIcon class="text-neutral-800" :data="data" />
+        <GitHubIcon class="text-neutral-800 transition duration-500 hover:rotate-6" :data="data" />
       </ToolTip>
       <ToolTip :name="'Linkedin'">
-        <LinkedinIcon class="text-blue-700" :data="data" />
+        <LinkedinIcon class="text-blue-700 transition duration-500 hover:rotate-6" :data="data" />
       </ToolTip>
     </div>
   </div>
